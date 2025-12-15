2b3a3e5519c195e0afddc6eb689d53677b51ee96
Leshui He <lhe@bates.edu>
2025-12-14 22:26:46 -0500

    completed 1. theory - exercise 02

--- 1_theory_tools/exercises/ex_ante/Exercise_2.py
+++ 1_theory_tools/exercises/ex_ante/Exercise_2.py
@@ -16,9 +16,12 @@ def solve_VFI(par):
         V_next = V_now.copy()
         for w in range(par.W+1):
             #Fill in
-            
-            
-            
+            c = np.arange(w+1)  #(w+1) vector of possible consumption choices given cake size w
+            V_next_c = V_next[w - c ]  #(w+1) vector of value next period given consumption c
+            V_guess = np.sqrt(c)+par.beta*V_next_c #(w+1) vector of possible values next period
+            V_now[w] = np.amax(V_guess) #Find the maximum value
+            Cstar[w] = np.argmax(V_guess) #Find the corresponding consumption
+            ## fill in end ##
             
         delta = np.amax(np.abs(V_now - V_next))
     
